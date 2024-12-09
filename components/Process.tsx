cefed5a38376b17c8fab8e18c4b273dd93e5ed3b
Manish <manishguhe301@gmail.com>
2024-12-10 00:52:44 +0530

    fix: responsiveness

--- components/Process.tsx
+++ components/Process.tsx
@@ -8,17 +8,17 @@ const Process = () => {
         background:
           'linear-gradient(180deg, rgba(238, 65, 151, 0.04) 0%, rgba(255, 255, 255, 0.04) 56.61%) no-repeat',
       }}
-      className=' h-[400px] flex flex-col items-center justify-center py-8'
+      className=' h-[400px] flex flex-col items-center justify-center py-8 max-sm:h-full'
     >
       <div className='text-center'>
         <h3 className='text-3xl font-bold text-[#393939]'>
           On s’occupe de <span className='text-[#5CD2DD]'>tout</span>
         </h3>
-        <p className='text-[#9C9C9C] font-normal text-sm py-2'>
+        <p className='text-[#9C9C9C] font-normal text-sm py-2 max-sm:px-2'>
           Office ipsum you must be muted. It meeting commitment busy pain.
         </p>
       </div>
-      <div className=' py-10 flex items-center  gap-5'>
+      <div className=' py-10 flex items-center  gap-5 max-sm:flex-col'>
         <div className=' flex flex-col items-center gap-2'>
           <Image src='/images/bag.svg' alt='' width={50} height={50} />
           <p className='font-medium text-base text-[#393939]'>
@@ -28,7 +28,7 @@ const Process = () => {
             Selon vos besoins
           </p>
         </div>
-        <div className='h-[1px] w-[100px] bg-[#E5E5E5]'></div>
+        <div className='h-[1px] w-[100px] bg-[#E5E5E5] max-sm:hidden'></div>
         <div className=' flex flex-col items-center gap-2'>
           <Image src='/images/dish.svg' alt='' width={50} height={50} />
           <p className='font-medium text-base text-[#393939]'>Nettoyage</p>
@@ -36,8 +36,7 @@ const Process = () => {
             Selon vos besoins
           </p>
         </div>
-        <div className='h-[1px] w-[100px] bg-[#E5E5E5]'></div>
-
+        <div className='h-[1px] w-[100px] bg-[#E5E5E5] max-sm:hidden'></div>
         <div className=' flex flex-col items-center gap-2'>
           <Image src='/images/web-store.svg' alt='' width={50} height={50} />
           <p className='font-medium text-base text-[#393939]'>
@@ -47,7 +46,7 @@ const Process = () => {
             Tout est possible
           </p>
         </div>
-        <div className='h-[1px] w-[100px] bg-[#E5E5E5]'></div>
+        <div className='h-[1px] w-[100px] bg-[#E5E5E5] max-sm:hidden  '></div>
         <div className=' flex flex-col items-center gap-2'>
           <Image src='/images/car.svg' alt='' width={50} height={50} />
           <p className='font-medium text-base text-[#393939]'>
