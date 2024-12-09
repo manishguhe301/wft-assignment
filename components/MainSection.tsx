b96f63bdeee3844d4626c67d014260465e79b1fd
Manish <manishguhe301@gmail.com>
2024-12-09 22:28:23 +0530

    feat: desc - prod

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,95 +1,109 @@
 import Image from 'next/image';
 import React from 'react';
+import ProductDesc from './ProductDesc';
 
 const MainSection = () => {
   return (
-    <div className='w-full flex gap-6'>
-      <div className='w-1/2 bg-[#F8F6F4] py-10 px-20 rounded-md relative'>
-        <div className='absolute top-2 left-2 flex gap-2 flex-col'>
-          {Array.from({ length: 4 }).map((_, index) => (
-            <div key={index} className='p-2 bg-white rounded-lg'>
+    <>
+      <div className='w-full flex gap-6'>
+        <div className='w-1/2 bg-[#F8F6F4] py-10 px-20 rounded-md relative'>
+          <div className='absolute top-2 left-2 flex gap-2 flex-col'>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <div key={index} className='p-2 bg-white rounded-lg'>
+                <Image
+                  src='/images/table.webp'
+                  alt='table'
+                  width={40}
+                  height={40}
+                  className={`w-10 h-10 ${index === 0 ? '' : 'opacity-50'}`}
+                />
+              </div>
+            ))}
+          </div>
+          <Image
+            src='/images/machine.webp'
+            alt='machine'
+            width={600}
+            height={400}
+            layout='responsive'
+            className='mix-blend-multiply'
+          />
+        </div>
+        <div className='w-1/2 flex flex-col justify-between'>
+          <div>
+            <div className=' flex items-center justify-between'>
+              <h1 className='text-3xl font-playfair text-[#111928]'>
+                Cheese – appareil à raclette 1/2 roue
+              </h1>
               <Image
-                src='/images/table.webp'
-                alt='table'
-                width={40}
-                height={40}
-                className={`w-10 h-10 ${index === 0 ? '' : 'opacity-50'}`}
+                src='/images/heart.svg'
+                alt='heart'
+                width={30}
+                height={30}
               />
             </div>
-          ))}
-        </div>
-        <Image
-          src='/images/machine.webp'
-          alt='machine'
-          width={600}
-          height={400}
-          layout='responsive'
-          className='mix-blend-multiply'
-        />
-      </div>
-      <div className='w-1/2 flex flex-col justify-between'>
-        <div>
-          <div className=' flex items-center justify-between'>
-            <h1 className='text-3xl font-playfair text-[#111928]'>
-              Cheese – appareil à raclette 1/2 roue
-            </h1>
-            <Image src='/images/heart.svg' alt='heart' width={30} height={30} />
-          </div>
-          <div className='flex items-center gap-2 my-3'>
-            <p className='font-geist text-2xl text-[#111928]'>39,50€</p>
-            <span className='text-[#9C9C9C] text-sm'>/pièce</span>
-          </div>
-          <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40'>
-            {/* Line */}
-          </div>
-          <div className='flex items-center justify-between'>
-            <div className=' flex items-center gap-4 font-geist py-2'>
-              <div className='flex items-center gap-2'>
-                <Image src='/images/scale.svg' alt='' width={24} height={24} />
-                <span>
-                  20<sup>cm</sup>
-                </span>
-              </div>
-              <div className='flex items-center gap-2'>
-                <Image src='/images/res.svg' alt='' width={24} height={24} />
-                <span>
-                  50<sup>cm</sup>
-                </span>
-              </div>
+            <div className='flex items-center gap-2 my-3'>
+              <p className='font-geist text-2xl text-[#111928]'>39,50€</p>
+              <span className='text-[#9C9C9C] text-sm'>/pièce</span>
             </div>
-            <p className='text-xs text-[#9C9C9C] font-geist'>RÉF : VABGN5</p>
-          </div>
-          <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40'>
-            {/* Line */}
-          </div>
-          <div className='w-1/2 font-geist text-sm  text-[#5D5D5D]'>
-            Location appareil à raclette - Raclette traditionnelle 1/2 roue{' '}
-            <br />
-            Réglable en hauteur <br /> Appareil à raclette professionnel <br />{' '}
-            Boîtier de chauffe horizontal réglable en hauteur
-            <span>
-              220V <br />
-              900W
-            </span>
-          </div>
-        </div>
-        <div>
-          <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40 self-end'>
-            {/* Line */}
-          </div>
-          <div className='flex items-center gap-2'>
-            <div className='flex items-center gap-10 border-2 border-[#9C9C9C] rounded-lg py-3 px-6 border-opacity-50'>
-              <span className='text-base font-medium'>-</span>
-              <span className='text-base font-medium'>1</span>
-              <span className='text-base font-medium'>+</span>
+            <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40'>
+              {/* Line */}
             </div>
-            <button className='text-[#fff] text-opacity-100 bg-[#5CD2DD] py-3 border font-bold uppercase  flex items-center justify-center w-full rounded-lg'>
-              Ajouter au panier
-            </button>
+            <div className='flex items-center justify-between'>
+              <div className=' flex items-center gap-4 font-geist py-2'>
+                <div className='flex items-center gap-2'>
+                  <Image
+                    src='/images/scale.svg'
+                    alt=''
+                    width={24}
+                    height={24}
+                  />
+                  <span>
+                    20<sup>cm</sup>
+                  </span>
+                </div>
+                <div className='flex items-center gap-2'>
+                  <Image src='/images/res.svg' alt='' width={24} height={24} />
+                  <span>
+                    50<sup>cm</sup>
+                  </span>
+                </div>
+              </div>
+              <p className='text-xs text-[#9C9C9C] font-geist'>RÉF : VABGN5</p>
+            </div>
+            <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40'>
+              {/* Line */}
+            </div>
+            <div className='w-[60%] font-geist text-sm  text-[#5D5D5D]'>
+              Location appareil à raclette - Raclette traditionnelle 1/2 roue{' '}
+              <br />
+              Réglable en hauteur <br /> Appareil à raclette professionnel{' '}
+              <br /> Boîtier de chauffe horizontal réglable en hauteur
+              <span>
+                220V <br />
+                900W
+              </span>
+            </div>
+          </div>
+          <div>
+            <div className=' h-[0.5px] w-full bg-[#9C9C9C] my-3 opacity-40 self-end'>
+              {/* Line */}
+            </div>
+            <div className='flex items-center gap-2'>
+              <div className='flex items-center gap-10 border-2 border-[#9C9C9C] rounded-lg py-3 px-6 border-opacity-50'>
+                <span className='text-base font-medium'>-</span>
+                <span className='text-base font-medium'>1</span>
+                <span className='text-base font-medium'>+</span>
+              </div>
+              <button className='text-[#fff] text-opacity-100 bg-[#5CD2DD] py-3 border font-bold uppercase  flex items-center justify-center w-full rounded-lg'>
+                Ajouter au panier
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ProductDesc />
+    </>
   );
 };
 
---
+++ components/ProductDesc.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React from 'react';
+
+const ProductDesc = () => {
+  return (
+    <div className='my-10 flex items-center gap-5'>
+      <div className='flex flex-col gap-2 w-1/2'>
+        <h3 className='text-xl font-normal text-[#111928]'>
+          Description produit
+        </h3>
+        <p className='text-sm text-[#9C9C9C] tracking-wide'>
+          Festi vous propose à la location un/une &quot;Jewel – grand
+          couteau/10pc&quot; pour votre évenement et ce dès 0,35 € / pièce
+          (HTVA). Que ce soit pour votre mariage, une fête d&apos;anniversaire
+          ou du personnel, ce produit a fait l&apos;objet d&apos;une sélection
+          rigoureuse par notre équipe. Il est en location chez nous sous la
+          référence &quot;VAJGC&quot;. Nous sommes à votre disposition pour que
+          les événements de nos clients, même en last-minute, soient toujours
+          une réussite. Vous pourrez trouver tout une série d&apos;autre produit
+          à louer de ce type dans la catégorie &quot;Art de la Table&quot;.
+        </p>
+      </div>
+      <div className='w-1/2 bg-[#FBF9F899]'>
+        <div className='py-[18px] w-full px-5 flex items-center justify-between'>
+          <p className='font-medium text-[#393939] text-base text-opacity-75'>Livraisons</p>
+          <Image src='/images/plus.svg' alt='' width={20} height={20} />
+        </div>
+        <div className='py-[18px] w-full px-5 flex items-center justify-between'>
+          <p className='font-medium text-[#393939] text-base text-opacity-75'>Questions</p>
+          <Image src='/images/plus.svg' alt='' width={20} height={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDesc;
