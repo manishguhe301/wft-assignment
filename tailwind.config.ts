40eb884fc1f10488b315457597e7dd0b9e265f6a
Manish <manishguhe301@gmail.com>
2024-12-09 17:50:38 +0530

    feat: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      }
     },
   },
   plugins: [],
